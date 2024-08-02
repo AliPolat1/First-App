59aa08a7d42dea560f94bd4028b7ba5c0a485089
Ali Polat <alifiratpolat992@gmail.com>
2024-08-02 16:04:01 +0300

    Video 1 ve 2

---
+++ script.js
@@ -0,0 +1,117 @@
+/*
+console.log("Test");
+
+// burada typeof kullanarak veri tiplerini deneyip öğrendim
+console.log( typeof "Test")
+console.log( typeof null)
+console.log( typeof true)
+console.log( typeof 5)
+console.log( typeof undefined)
+
+*/
+
+
+// let ile sabit olmayan daha sonra değiştirilebilir değişkenlerimi tanımladım
+/*
+let ad;
+let soyad;
+let yas;
+let cuzdan;
+let varlik;
+let yasam;
+
+
+// burada tanımladığım değişkenlere veri girişleri yaptım
+
+ad = "Ali";
+soyad = "Polat"
+yas = 3
+cuzdan = null
+varlik = undefined
+yasam = true
+
+// burada ise değişkenlerimi güzelce tarayıcımın konsoluna yazdırıyorum
+
+console.log(ad + " " + soyad)
+console.log("Yaş: " + yas)
+console.log("Para miktarı: " + cuzdan)
+console.log("Mal varlığı: " + varlik)
+console.log("Hayattamı? : " + yasam)
+
+*/
+
+
+// Eşitlik kontrolleri 
+/* 
+let string = "Ali"
+let int = 1
+
+let int2 = 1
+let string2 = "Ali"
+
+console.log("İnt / string eşitliği ")
+console.log(int == string)
+console.log("İnt / int eşitliği ") 
+console.log(int == int2)
+console.log("String / string eşitliği ")
+console.log(string == string2)
+
+*/
+
+// Array / Diziler
+
+let sayilar = ["selam",1,true,undefined]
+
+//console.log(typeof sayilar[0], typeof sayilar[1], typeof sayilar[2], typeof sayilar[3])
+//console.log( sayilar[0],  sayilar[1],  sayilar[2],  sayilar[3])
+
+//console.log(sayilar.length)
+
+/*
+let ben = {
+    ad: "Ali",
+    soyad: "Polat"
+}
+
+let birisi = {
+    ad: "Awdaw",
+    soyad: "Polat"
+}
+console.log( ben.soyad == birisi.soyad)
+
+console.log(ben == birisi)
+birisi.soyad = "Polay"
+console.log( ben.ad == birisi.ad)
+console.log( ben.soyad == birisi.soyad)
+*/
+
+//Math. Kullanımı 
+/*
+console.log(Math.round(3.50))
+console.log(Math.floor(3.50))
+console.log(Math.ceil(3.1))
+console.log(Math.min(-5,10,50,30,20,15))
+console.log(Math.max(-5,10,50,30,20,15))
+
+for ( let i = 0; i < 5 ; i++){
+    console.log(Math.random())
+}
+console.log("   ")
+
+for ( let i = 0; i < 5 ; i++){
+    console.log(Math.floor(Math.random() * 11))
+}
+*/
+
+let ad = "  Ali Fırat  "
+console.log(ad)
+console.log(ad.toUpperCase())
+console.log(ad.toLowerCase())
+console.log(ad.split(' '))
+console.log(ad.trim())
+
+console.log(ad.includes("Fırat"))
+console.log(ad.replaceAll("Fırat", "Polat"))
+console.log(ad.indexOf("A"))
+console.log(ad.startsWith(" "))
+console.log(ad.endsWith("t"))
