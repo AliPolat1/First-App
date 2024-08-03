2f04a34d9f3afc9aa736e65d5652c16d096a1a65
AliPolat1 <alifiratpolat992@gmail.com>
2024-08-03 16:12:50 +0300

    Dizi kullanımı için örnekler

--- video5.js
+++ video5.js
@@ -1,4 +1,5 @@
-const sayilar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Ali"]
+let sayilar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Ali"]
+let bosdizi = []
 console.log(sayilar)
 console.log(sayilar.length);
 console.log(sayilar[0])
@@ -8,6 +9,13 @@ console.log(sayilar.sort())
 sayilar.push(36)
 console.log(sayilar)
 
+let sonindex = sayilar.length -1
+console.log(sayilar[sonindex])
 
+sayilar[sonindex] = 35
+console.log(sayilar[sonindex])
+
+bosdizi = Array(8).fill("X")
+console.log(bosdizi)
 
 
