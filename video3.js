b356822986aca56ca15abd85d996f51157893782
Ali Polat <alifiratpolat992@gmail.com>
2024-08-03 14:48:48 +0300

    Date işlemleri / kullanıcı girişi alma / confirm

--- video3.js
+++ video3.js
@@ -1,3 +1,5 @@
+const onay = confirm('Bu kodları test etmek istediğine emin misin?')
+
 let benmiyim = true
 let dogru = 4 > 3
 let yanlis = 4 == 3
@@ -17,3 +19,31 @@ let b = 6
 
 
 //console.log( "Ali".lenght == "Mehmet".length )
+
+
+let tarih = new Date()
+
+console.log("Tam Tarih: "+ tarih.getDate())
+console.log("Yıl: "+ tarih.getFullYear())
+console.log("Ay: "+ tarih.getMonth())
+console.log("Gün: "+ tarih.getDay())
+console.log("Saat: "+ tarih.getHours()+":"+tarih.getMinutes())
+
+// veri girişi alma
+
+//let sayi1 = parseInt(prompt("Sayi 1: "))
+//let sayi2 = parseInt(prompt("Sayi 2: "))
+//let toplam = sayi1 + sayi2
+//console.log("Toplam= " + toplam)
+
+
+// girişe göre işlem yapma 
+
+let sayi1 = parseInt(prompt("Sayi 1: "))
+let sayi2 = parseInt(prompt("Sayi 2: "))
+
+let toplam = sayi1 + sayi2
+console.log("Cevap= " + toplam)
+    
+
+
