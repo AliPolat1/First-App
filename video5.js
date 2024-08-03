e603997a7de697d061a1c85c1daa65c506aa2522
Ali Polat <alifiratpolat992@gmail.com>
2024-08-03 16:01:51 +0300

    Dizi kullanımı için örnekler

---
+++ video5.js
@@ -0,0 +1,13 @@
+const sayilar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Ali"]
+console.log(sayilar)
+console.log(sayilar.length);
+console.log(sayilar[0])
+console.log(sayilar[10])
+console.log(sayilar.reverse())
+console.log(sayilar.sort())
+sayilar.push(36)
+console.log(sayilar)
+
+
+
+
